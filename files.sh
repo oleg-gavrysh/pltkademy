564624d9dd6e7cf5596db6f78e82b98a510793ee
oleg-gavrysh <78376473+oleg-gavrysh@users.noreply.github.com>
2021-08-22 13:33:33 +0300

    Update files.sh

--- files.sh
+++ files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+TIMESTAMP=`date +%T`
+
 for NUM in `echo {1..10}`
 do touch test_$NUM.txt
-< /dev/urandom tr -cd "[:print:]" | head -c 1000 > test_$NUM.txt
+< /dev/urandom tr -cd "[:print:]" | head -c 1000 > test_$TIMESTAMP_$NUM.txt
 done
